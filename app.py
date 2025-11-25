57dbf7c401fc4b6590d31838cbefacc74d67348d
prem <thummaluruprem@gmail.com>
2025-11-25 16:20:45 +0530

    fourth version

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-a = 7
-if a % 2 == 0:
+num = input("enter a number")
+if num % 2 == 0:
      print("even number")
 else:
     print("odd number")
-num = 7
-      if a % 2 == 0:
-       print("even number")
+
 
